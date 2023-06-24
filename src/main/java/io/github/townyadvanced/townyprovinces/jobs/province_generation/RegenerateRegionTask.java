ae20efa0eed65f178fa25bea2930a4afc132249b
Goosius1 <goosius1@gmail.com>
2023-06-24 19:48:19 +0100

    small usabliity improveent

--- src/main/java/io/github/townyadvanced/townyprovinces/jobs/province_generation/RegenerateRegionTask.java
+++ src/main/java/io/github/townyadvanced/townyprovinces/jobs/province_generation/RegenerateRegionTask.java
@@ -42,12 +42,12 @@ public class RegenerateRegionTask extends BukkitRunnable {
 	public void run() {
 		try {
 			TownyProvinces.info("Regeneration Job Started");
-			TownyProvinces.info("Regeneration Job: Acquiring dynmap display lock");
-			synchronized (TownyProvinces.DYNMAP_DISPLAY_LOCK) {
-				TownyProvinces.info("Regeneration Job: Dynmap display lock acquired");
-				TownyProvinces.info("Regeneration Job: Acquiring land validation lock");
-				synchronized (TownyProvinces.LAND_VALIDATION_LOCK) {
-					TownyProvinces.info("Regeneration Job: Land Validation lock acquired");
+			TownyProvinces.info("Regeneration Job: Acquiring land validation lock");
+			synchronized (TownyProvinces.LAND_VALIDATION_LOCK) {
+				TownyProvinces.info("Regeneration Job: Land Validation lock acquired");
+				TownyProvinces.info("Regeneration Job: Acquiring dynmap display lock");
+				synchronized (TownyProvinces.DYNMAP_DISPLAY_LOCK) {
+					TownyProvinces.info("Regeneration Job: Dynmap display lock acquired");
 					executeRegionRegenerationJob();
 				}
 			}
